3ed3abf452c2cce2caf2307c41a0c99525d1cbc0
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 16:42:31 +0200

    Update Pattedyr.cs
    Tilføjede køn til constructor

--- Zoo_ConsoleApp/Pattedyr.cs
+++ Zoo_ConsoleApp/Pattedyr.cs
@@ -7,7 +7,7 @@ namespace Zoo_ConsoleApp
     public class Pattedyr : Dyr
     {
         public bool LæggerÆg { get; }
-        public Pattedyr(string navn, double vægt, bool læggerÆg) : base(navn, vægt)
+        public Pattedyr(string navn, double vægt, string køn, bool læggerÆg) : base(navn, vægt, køn)
         {
             læggerÆg = LæggerÆg;
         }
