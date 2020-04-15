9f1bbbb5e1cc1ccb64c6f2c95c4a5d5b13bb8ee3
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:25:53 +0200

    Update Ørred.cs
    Tilføjede interface IByttedyr

--- Zoo_ConsoleApp/Ørred.cs
+++ Zoo_ConsoleApp/Ørred.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    public class Ørred : Fisk
+    public class Ørred : Fisk, IByttedyr
     {
         public Ørred(double vægt, string køn, int længde) : base("Ørred", vægt, køn, længde)
         {
 
+        }
+        public bool ErByttedyr()
+        {
+            return true;
         }
     }
 }
