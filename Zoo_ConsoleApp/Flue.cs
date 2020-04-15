d7a32451145e033b224030a759b060eb782cfdc1
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:23:29 +0200

    Update Flue.cs
    Tilføjede interface IByttedyr

--- Zoo_ConsoleApp/Flue.cs
+++ Zoo_ConsoleApp/Flue.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    public class Flue : Insekt
+    public class Flue : Insekt, IByttedyr
     {
         public Flue(double vægt, string køn) : base("Flue", vægt, køn, false)
         {
 
+        }
+        public bool ErByttedyr()
+        {
+            return true;
         }
     }
 }
