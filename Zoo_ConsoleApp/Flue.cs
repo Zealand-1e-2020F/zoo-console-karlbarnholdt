6adbbb1aa114eaf2f229e4c9792cd1c69a12b971
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 17:30:20 +0200

    Create Flue.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Flue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    class Flue : Insekt
+    {
+        public Flue(double vægt, string køn) : base("Flue", vægt, køn, false)
+        {
+
+        }
+    }
+}
