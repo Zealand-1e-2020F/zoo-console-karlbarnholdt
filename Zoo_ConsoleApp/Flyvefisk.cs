c3d14c64a3e223a40efb4cc99a21d211caab4eb0
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:23:56 +0200

    Update Flyvefisk.cs
    Tilføjede interface IByttedyr

--- Zoo_ConsoleApp/Flyvefisk.cs
+++ Zoo_ConsoleApp/Flyvefisk.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    class Flyvefisk : Fisk
+    class Flyvefisk : Fisk, IByttedyr
     {
         public Flyvefisk(double vægt, string køn, int længde) : base("Flyvefisk", vægt, køn, længde)
         {
         
+        }
+        public bool ErByttedyr()
+        {
+            return true;
         }
     }
 }
