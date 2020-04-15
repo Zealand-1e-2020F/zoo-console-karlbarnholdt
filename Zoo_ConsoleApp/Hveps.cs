8072a47b205eb24c714cefb032ba906fd78b3a33
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:24:33 +0200

    Update Hveps.cs
    Tilføjede interface IByttedyr

--- Zoo_ConsoleApp/Hveps.cs
+++ Zoo_ConsoleApp/Hveps.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    public class Hveps : Insekt
+    public class Hveps : Insekt, IByttedyr
     {
         public Hveps(double vægt, string køn) : base("Hveps", vægt, køn, true)
         {
 
+        }
+        public bool ErByttedyr()
+        {
+            return true;
         }
     }
 }
