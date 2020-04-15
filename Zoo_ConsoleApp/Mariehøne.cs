3ab609e8c53f97666500746d2ca883cb8b56d992
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 17:44:34 +0200

    Create Mariehøne.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Mariehøne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    class Mariehøne : Insekt
+    {
+        public Mariehøne(double vægt, string køn) : base("Mariehøne", vægt, køn, false)
+        {
+
+        }
+    }
+}
