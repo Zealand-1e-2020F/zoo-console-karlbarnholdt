966cf7b8ff12dbea3f39f094d470cdee9bf89e31
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:21:41 +0200

    Update Tiger.cs
    Tilføjede interface IRovdyr

--- Zoo_ConsoleApp/Tiger.cs
+++ Zoo_ConsoleApp/Tiger.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    public class Tiger : Pattedyr
+    public class Tiger : Pattedyr, IRovdyr
     {
         public Tiger(double vægt, string køn) : base("Tiger", vægt, køn, false)
         {
             
+        }
+        public bool ErRovdyr()
+        {
+            return true;
         }
     }
 }
