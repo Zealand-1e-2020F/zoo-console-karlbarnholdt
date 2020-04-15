622e664518fe12efc620d9f4e66113985ae29a3c
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-04-15 09:35:11 +0200

    Add project files.

---
+++ Zoo_ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zoo_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dyr elefant = new Dyr("Elefant");
+
+            Console.WriteLine(elefant.DyreNavn);
+
+        }
+    }
+}
---
+++ Zoo_ConsoleApp/dyr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    /// <summary>
+    /// base klasse for dyr i zoo
+    /// </summary>
+    public class Dyr
+    {
+        public string DyreNavn { get; }
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Dyr()
+        {
+
+        }
+
+        public Dyr(string navn)
+        {
+            this.DyreNavn = navn;
+
+        }
+
+
+    }
+}
