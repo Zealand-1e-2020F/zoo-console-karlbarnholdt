e78c139a35ce8e0346ea3319079a1468a63636b1
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 17:41:16 +0200

    Create Myg.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Myg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    class Myg : Insekt
+    {
+        public Myg(double vægt, string køn) : base("Myg", vægt, køn, false)
+        {
+
+        }
+    }
+}
