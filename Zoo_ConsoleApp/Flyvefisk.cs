f9f1ebc875bfd88fcfb43cdfacea7c190a500aa8
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 17:50:27 +0200

    Create Flyvefisk.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Flyvefisk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    class Flyvefisk : Fisk
+    {
+        public Flyvefisk(double vægt, string køn, int længde) : base("Flyvefisk", vægt, køn, længde)
+        {
+        
+        }
+    }
+}
