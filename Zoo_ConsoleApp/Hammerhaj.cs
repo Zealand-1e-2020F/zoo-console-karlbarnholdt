36c0bf6a0bf20c31245d1e8526e56ce1021c845b
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 17:56:38 +0200

    Create Hammerhaj.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Hammerhaj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    public class Hammerhaj : Fisk
+    {
+        public Hammerhaj(double vægt, string køn, int længde) : base("Hammerhaj", vægt, køn, længde)
+        {
+
+        }
+    }
+}
