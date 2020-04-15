ae51639a908f390780bb2e6785f47c6a1668ef0a
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:36:52 +0200

    Update Program.cs
    Fjernede metodekald Console.WriteLine for hvert objekt.
    Tilføjede en liste af typen Dyr.
    Tilføjede samtlige objekter til listen.
    Tilføjede foreach loop til metodekald af Console.WriteLine.

--- Zoo_ConsoleApp/Program.cs
+++ Zoo_ConsoleApp/Program.cs
@@ -8,43 +8,39 @@ namespace Zoo_ConsoleApp
         static void Main(string[] args)
         {
             Elefant elefant = new Elefant(5000, "Han", 60);
-            Console.WriteLine(elefant);
-
             Tiger tiger = new Tiger(120, "Han");
-            Console.WriteLine(tiger);
-
             Næbdyr næbdyr = new Næbdyr(28, "Han");
-            Console.WriteLine(næbdyr);
-
             Bjørn bjørn = new Bjørn(190, "Hun", false);
-            Console.WriteLine(bjørn);
-
             Bjørn peterPlys = new Bjørn(45, "Han", true);
-            Console.WriteLine(peterPlys);
-
             Flue flue = new Flue(0.002, "Han");
-            Console.WriteLine(flue);
-
             Hveps hveps = new Hveps(0.003, "Hun");
-            Console.WriteLine(hveps);
-
             Myg myg = new Myg(0.001, "Han");
-            Console.WriteLine(myg);
-
             Mariehøne mariehøne = new Mariehøne(0.0035, "Han");
-            Console.WriteLine(mariehøne);
-
             Flyvefisk flyvefisk = new Flyvefisk(0.1, "Hun", 28);
-            Console.WriteLine(flyvefisk);
-
             Hammerhaj hammerhaj = new Hammerhaj(280, "Hun", 220);
-            Console.WriteLine(hammerhaj);
-
             Dragefisk dragefisk = new Dragefisk(2, "Han", 80);
-            Console.WriteLine(dragefisk);
-
             Ørred ørred = new Ørred(2.2, "Hun", 75);
-            Console.WriteLine(ørred);
+
+            List<Dyr> dyr = new List<Dyr>();
+
+            dyr.Add(elefant);
+            dyr.Add(tiger);
+            dyr.Add(næbdyr);
+            dyr.Add(bjørn);
+            dyr.Add(peterPlys);
+            dyr.Add(flue);
+            dyr.Add(hveps);
+            dyr.Add(myg);
+            dyr.Add(mariehøne);
+            dyr.Add(flyvefisk);
+            dyr.Add(hammerhaj);
+            dyr.Add(dragefisk);
+            dyr.Add(ørred);
+
+            foreach (var item in dyr)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
