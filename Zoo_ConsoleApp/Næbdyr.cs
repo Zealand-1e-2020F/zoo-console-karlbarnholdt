fea8611cfde77cba65cd5e99d0928135cbf9689c
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:21:02 +0200

    Update Næbdyr.cs
    Tilføjede interface IRovdyr

--- Zoo_ConsoleApp/Næbdyr.cs
+++ Zoo_ConsoleApp/Næbdyr.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace Zoo_ConsoleApp
 {
-    public class Næbdyr : Pattedyr
+    public class Næbdyr : Pattedyr, IRovdyr
     {
         public Næbdyr(double vægt, string køn) : base("Næbdyr", vægt, køn, true)
         {
 
+        }
+        public bool ErRovdyr()
+        {
+            return true;
         }
     }
 }
