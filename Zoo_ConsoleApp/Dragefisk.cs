184f32f76ede2aa711f3578736d53f721c05f1d3
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 18:03:41 +0200

    Create Dragefisk.cs
    Tilføjede constructor

---
+++ Zoo_ConsoleApp/Dragefisk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_ConsoleApp
+{
+    public class Dragefisk : Fisk
+    {
+        public Dragefisk(double vægt, string køn, int længde) : base("Dragefisk", vægt, køn, længde)
+        {
+
+        }
+    }
+}
