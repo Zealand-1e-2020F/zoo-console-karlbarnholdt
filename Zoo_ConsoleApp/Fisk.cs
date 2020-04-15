46db85c90ac2df985b9bb764cc905db0aba99221
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2020-04-15 16:48:05 +0200

    Update Fisk.cs
    Tilføjede køn til constructor

--- Zoo_ConsoleApp/Fisk.cs
+++ Zoo_ConsoleApp/Fisk.cs
@@ -6,10 +6,10 @@ namespace Zoo_ConsoleApp
 {
     public class Fisk : Dyr
     {
-        public int Lenght { get; }
-        public Fisk(string navn, int længde, double vægt) : base(navn, vægt)
+        public int Længde { get; }
+        public Fisk(string navn, double vægt, string køn, int længde) : base(navn, vægt, køn)
         {
-            længde = Lenght;
+            Længde = længde;
         }
     }
 }
